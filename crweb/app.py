c78b36f762427deab6c8d219be450808ee50682b
gabrielmonzato20 <gabrielmonzato20@gmail.com>
2018-08-29 07:59:02 -0300

    alt9

--- crweb/app.py
+++ crweb/app.py
@@ -1,8 +1,9 @@
 # app.py
 from flask import Flask
+from web import takedata
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "<h1>Hello Melody :)</h1>"
+    return return jsonify(Produtos=takedata())
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
--- crweb/web.py
+++ crweb/web.py
@@ -1,14 +1,18 @@
 from bs4 import BeautifulSoup as take
 import requests as ree
-def takedata(url):
+def takedata(url,wordd):
     l=[]
     for c in url:
         re = ree.get(c)
         so = take(re.text,"html.parser")
         lista = str(so.body).split()
         cont=0
+		li= { }
         for word in lista:
-            if word == 'abacaxi':
-                cont+=1
-        print(cont)
+		if wordd == word:
+			cont+=1
+		li["link"] = str(c)
+		li[word] = cont
+        		
+          
 takedata(['https://pt.wikipedia.org/wiki/Anan%C3%A1s'])
