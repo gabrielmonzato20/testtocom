2875fbd6477caaeb4f8a86e8bb184a4176fc7fbf
Gabriel Monzato <gabrielmonzato20@gmail.com>
2018-08-28 22:42:34 -0300

    alt4

---
+++ crweb/app.py
@@ -0,0 +1,8 @@
+# app.py
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "<h1>Hello Melody :)</h1>"
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
